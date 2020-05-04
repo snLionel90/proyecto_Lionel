a10361d4d7ee93ceadd1810b4d1115d2d09a33db
snLionel90 <leonjuman@gmail.com>
2020-05-04 17:33:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button continuar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        continuar = findViewById(R.id.button);
+        continuar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (continuar.isClickable()) {
+            Intent intent = new Intent(this, panelRadio.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menus_superior,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+        if (id==R.id.item_creator){
+            Toast.makeText(this, "Aplicacion creada por:Lionel Sanchez(c)2020",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id==R.id.i_version){
+            Toast.makeText(this, "Version de la aplicacion: 1.0.2020",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id==R.id.salir_it){
+            Toast.makeText(this, "Gracias por utilizar la aplicacion",Toast.LENGTH_LONG).show();
+            finish();
+            return true;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+}
