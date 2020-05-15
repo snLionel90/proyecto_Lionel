675242bb56eb4f9aed2ad1c17cead9d0678fcb2b
snLionel90 <leonjuman@gmail.com>
2020-05-15 21:40:50 +0200

    Crecion de la pantalla inicos de la radio e oinseccion de codigo.

--- app/src/main/java/com/example/myapplication/iinicios_de_la_radio.java
+++ app/src/main/java/com/example/myapplication/iinicios_de_la_radio.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class iinicios_de_la_radio extends AppCompatActivity implements View.OnClickListener {
     Button retroceso;
@@ -34,4 +37,32 @@ public class iinicios_de_la_radio extends AppCompatActivity implements View.OnCl
         }
 
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menus_superior,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+        if (id==R.id.item_creator){
+            Toast.makeText(this, "Aplicacion creada por:Lionel Sanchez(c)2020",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id==R.id.i_version){
+            Toast.makeText(this, "Version de la aplicacion: 1.0.2020",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id==R.id.salir_it){
+            Toast.makeText(this, "Gracias por utilizar la aplicacion",Toast.LENGTH_LONG).show();
+            finish();
+            return true;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+
+
+
 }
